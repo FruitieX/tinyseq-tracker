fcdd50b031a2ccedd3d6181c1e3d12b96057de7d
Henrik Hillner <henrik@hillner.fi>
2018-10-31 19:24:02 +0200

    Adds some grid layouting

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,11 +4,32 @@ import styled from 'styled-components';
 import song from '../../song.json';
 import { Instrument, parseSong, TSSong } from '../types/instrument';
 import { Track } from './Track';
+import SoundFactory from './SoundFactory';
 
 const NoteEditor = styled.div`
+  grid-area: main-editor;
   display: flex;
   flex-direction: row;
 `;
+
+const TrackerWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 6fr 3fr;
+  grid-template-rows: 150px 6fr 2fr;
+  height: 100vh;
+  grid-template-areas:
+    "panel header right-top"
+    "panel main-editor right-top"
+    "panel footer right-bottom";
+`
+
+const PatternWrapper = styled.div`
+  grid-area: panel;
+`
+
+const GraphWrapper = styled.div`
+  grid-area: header;
+`
 
 const renderTrack = (instrument: Instrument, index: number) => {
   return <Track key={index} index={index} instrument={instrument} />;
@@ -17,7 +38,14 @@ const renderTrack = (instrument: Instrument, index: number) => {
 export default class Editor extends React.Component {
   render() {
     return (
-      <NoteEditor>{parseSong(song as TSSong).map(renderTrack)}</NoteEditor>
+      <TrackerWrapper>
+        <PatternWrapper>Pootis patterns here</PatternWrapper>
+        <GraphWrapper></GraphWrapper>
+        <NoteEditor>
+          {parseSong(song as TSSong).map(renderTrack)}
+        </NoteEditor>
+        <SoundFactory></SoundFactory>
+      </TrackerWrapper>
     );
   }
 }
---
+++ src/components/SoundFactory.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SoundFactoryContainer = styled.div`
+  grid-area: right-top;
+`;
+
+export default class SoundFactory extends React.Component {
+  state = {value: ""};
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({value: event.target.value});
+  }
+  addSine = () => {
+    this.setState({value: this.state.value + "+Math.sin(2*2)"});
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addSine}>sin(ft)</button>
+        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+        {/* <span>{this.state.value}</span> */}
+      </div>
+    );
+  }
+}
+

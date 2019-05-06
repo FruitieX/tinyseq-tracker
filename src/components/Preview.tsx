4e8171c070adbff6020977281797deec57de7a03
Rasmus Eskola <fruitiex@gmail.com>
2019-05-06 17:00:31 +0300

    Log errors directly in Preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -7,9 +7,29 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   grid-area: preview;
   display: flex;
+  position: relative;
 `;
 
+const Error = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  color: black;
+  opacity: 0.75;
+  overflow: auto;
+`;
+
+const ErrorText = styled.pre`
+  padding: 32px;
+`;
+
+// Set by demo to return value of requestAnimationFrame
+let Z: number = -1;
+
 export const Preview: React.FunctionComponent = () => {
+  const [compileError, setError] = React.useState<string>();
+
   React.useEffect(() => {
     const fragmentShader = require('../fragment.glsl');
     const vertexShader = require('../vertex.glsl');
@@ -20,12 +40,18 @@ export const Preview: React.FunctionComponent = () => {
 
     console.log('running initDemo');
 
-    // work around strict mode ;)
-    Function(src)();
+    try {
+      // work around strict mode ;)
+      Function(src)();
+
+      setError(undefined);
+    } catch (e) {
+      console.error(e);
+      setError(e);
+    }
 
     // Cleanup
     return () => {
-      // @ts-ignore: Z is set by the demo
       cancelAnimationFrame(Z);
     };
   });
@@ -33,6 +59,11 @@ export const Preview: React.FunctionComponent = () => {
   return (
     <Wrapper>
       <canvas id="W" />
+      {compileError && (
+        <Error>
+          <ErrorText>{compileError}</ErrorText>
+        </Error>
+      )}
     </Wrapper>
   );
 };
